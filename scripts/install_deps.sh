4c42f8b307b5a6eac8c94690c5ff8af2d3429f44
P. D. Finn KD9WEH <pdfinn@users.noreply.github.com>
2025-05-21 12:41:32 +0700

    Update install_deps.sh

--- scripts/install_deps.sh
+++ scripts/install_deps.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 # Install dependencies for building Gammera on Debian/Ubuntu systems
+
 set -e
+
+# Install system dependencies
 sudo apt-get update
-sudo apt-get install -y plan9port gcc make
+sudo apt-get install -y gcc make x11-utils libx11-dev libxt-dev libxext-dev
+
+# Clone and install plan9port
+cd /usr/local/src
+sudo git clone https://github.com/9fans/plan9port.git
+cd plan9port
+sudo ./INSTALL
